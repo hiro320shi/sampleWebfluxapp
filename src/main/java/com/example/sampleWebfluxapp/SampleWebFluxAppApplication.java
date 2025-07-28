c40d508ec579c6f4d14f0f13f6ced389bdbf6060
hiro320shi <hiroshi.sei3@docomo.ne.jp>
2025-07-28 21:26:05 +0900

    first commit

---
+++ src/main/java/com/example/sampleWebfluxapp/SampleWebFluxAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.sampleWebfluxapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SampleWebFluxAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SampleWebFluxAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/sampleWebfluxapp/SampleWebFluxAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sampleWebfluxapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SampleWebFluxAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
